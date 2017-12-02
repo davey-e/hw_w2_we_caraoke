737e56f99480821d44cd4b2434fd328c5a2c8e4c
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-02 14:46:21 +0000

    Added a favourite_song attribute to the guest class

--- guest.rb
+++ guest.rb
@@ -1,11 +1,12 @@
 class Guest
 
-  attr_reader :first_name, :last_name, :wallet
+  attr_reader :first_name, :last_name, :wallet, :favourite_song
 
-  def initialize(first_name, last_name, wallet)
+  def initialize(first_name, last_name, wallet, favourite_song)
     @first_name = first_name
     @last_name = last_name
     @wallet = wallet
+    @favourite_song = favourite_song
   end
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -1,12 +1,15 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../guest")
+require_relative("../song")
 
 class TestGuest < MiniTest::Test
 
   def setup
-    @guest1 = Guest.new("Dave", "Ellis", 50)
-    @guest2 = Guest.new("Manny", "Chita", 100)
+    @fav_song1 = Song.new("Learn To Fly", "Foo Fighters")
+    @fav_song2 = Song.new("Wonderwall", "Oasis")
+    @guest1 = Guest.new("Dave", "Ellis", 50, @fav_song1)
+    @guest2 = Guest.new("Manny", "Chita", 100, @fav_song2)
   end
 
   def test_guest_has_first_name
@@ -24,6 +27,12 @@ class TestGuest < MiniTest::Test
   def test_guest_has_wallet
     expected = 50
     actual = @guest1.wallet
+    assert_equal(expected, actual)
+  end
+
+  def test_guest_has_favourite_song
+    expected = @fav_song1
+    actual = @guest1.favourite_song
     assert_equal(expected, actual)
   end
 
