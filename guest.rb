fc7addd7e9a53bd88f237a0e3c8bba756f7790e5
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-02 14:19:19 +0000

    Added a new wallet attribute to the guest class

--- guest.rb
+++ guest.rb
@@ -1,10 +1,11 @@
 class Guest
 
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :wallet
 
-  def initialize(first_name, last_name)
+  def initialize(first_name, last_name, wallet)
     @first_name = first_name
     @last_name = last_name
+    @wallet = wallet
   end
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -5,8 +5,8 @@ require_relative("../guest")
 class TestGuest < MiniTest::Test
 
   def setup
-    @guest1 = Guest.new("Dave", "Ellis")
-    @guest2 = Guest.new("Manny", "Chita")
+    @guest1 = Guest.new("Dave", "Ellis", 50)
+    @guest2 = Guest.new("Manny", "Chita", 100)
   end
 
   def test_guest_has_first_name
@@ -18,6 +18,12 @@ class TestGuest < MiniTest::Test
   def test_guest_has_last_name
     expected = "Ellis"
     actual = @guest1.last_name
+    assert_equal(expected, actual)
+  end
+
+  def test_guest_has_wallet
+    expected = 50
+    actual = @guest1.wallet
     assert_equal(expected, actual)
   end
 
